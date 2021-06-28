170a20531761f13e3bfc7b7026e60a1a409a2f68
kanij fatema laizu <kanij.fatema.laizu@gmail.com>
2021-06-28 16:00:21 +0600

    live

--- src/js/bootstrap-navbar.js
+++ src/js/bootstrap-navbar.js
@@ -52,13 +52,13 @@ const navbarInit = () =>{
      // Change navbar background color on scroll
      window.addEventListener(Events.SCROLL, () => {
       const { scrollTop } = html;
-      let alpha = (scrollTop / windowHeight) * .75;
+      let alpha = (scrollTop / windowHeight) * .15;
       // Add class on scroll
       navbar.classList.add('backdrop');
       if(alpha === 0){
         navbar.classList.remove('backdrop');
       }
-      alpha >= 1 && (alpha = 0.75);
+      alpha >= 1 && (alpha = 1);
       navbar.style.backgroundColor = `rgba(${colorRgb[0]}, ${colorRgb[1]}, ${colorRgb[2]}, ${alpha})`;
       navbar.style.backgroundImage = (alpha > 0 || utils.hasClass(navbarCollapse, 'show')) ? backgroundImage : 'none';
       (alpha > 0 || utils.hasClass(navbarCollapse, 'show')) ? navbar.classList.add(shadowName):navbar.classList.remove(shadowName);
@@ -113,4 +113,3 @@ const navbarInit = () =>{
 };
 
 export default navbarInit;
-
